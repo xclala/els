d819097dc562664376f357bdb6c4d4da24526fbc
xclala <3035699035@qq.com>
2020-10-07 14:01:06 +0800

    define shape constant

--- SourceCode/main.py
+++ SourceCode/main.py
@@ -34,3 +34,18 @@ LIGHTCOLORS = (LIGHTBLUE, LIGHTGREEN, LIGHTRED, LIGHTYELLOW)
 assert len(COLORS) == len(LIGHTCOLORS)
 TEMPLATEWIDTH = 5
 TEMPLATEHEIGHT = 5
+S_SHAPE_TEMPLATE = [['.....', '.....', '..OO.', '.OO..', '.....', ], [
+    '.....', '..O..', '..OO.', '...O.', '.....']]
+Z_SHAPE_TEMPLATE = [['.....', '.....', '.OO..', '..OO.', '.....'], [
+    '.....', '..O..', '.OO..', '.O...', '.....']]
+I_SHAPE_TEMPLATE = [['..O..', '..O..', '..O..', '..O..', '.....'], [
+    '.....', '..O..', 'OOOO.', '.....', '.....']]
+O_SHAPE_TEMPLATE = [['.....', '.....', '.OO..', '.OO..', '.....']]
+J_SHAPE_TEMPLATE = [['.....', '.O...', '.OOO.', '.....', '.....'], [
+    '.....', '..OO.', '..O..', '..O..', '.....'], ['.....', '.....', '.OOO.', '...O.', '.....'], ['.....', '..O..', '..O..', '.OO..', '.....']]
+L_SHAPE_TEMPLATE = [['.....', '...O.', '.OOO.', '.....', '.....'], [
+    '.....', '..O..', '..O..', '..OO.', '.....'], ['.....', '.....', '.OOO.', '.O...', '.....'], ['.....', '.OO..', '..O..', '..O..', '.....']]
+T_SHAPE_TEMPLATE = [['.....', '..O..', '.OOO.', '.....', '.....'], ['.....', '..O..', '..OO.', '..O..', '.....'], [
+    '.....', '.....', '.OOO.', '..O..', '.....'], ['.....', '..O..', '.OO..', '..O..', '.....']]
+PIECES = {'S': S_SHAPE_TEMPLATE, 'Z': Z_SHAPE_TEMPLATE, 'J': J_SHAPE_TEMPLATE,
+          'L': L_SHAPE_TEMPLATE, 'I': I_SHAPE_TEMPLATE, 'O': O_SHAPE_TEMPLATE, 'T': T_SHAPE_TEMPLATE}
